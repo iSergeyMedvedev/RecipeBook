3314839fed97377611973e1e85d75d30a2148f9c
Sergey Medvedev <devgod@mail.ru>
2024-01-02 12:24:12 +0300

    Initial Commit

---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Sergey Medvedev on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
